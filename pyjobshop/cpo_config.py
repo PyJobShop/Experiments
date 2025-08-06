fa198cc3d81bc881e0059e9d099bd789b7ad8cd4
Leon Lan <l.lan@vu.nl>
2025-08-06 21:22:41 +0200

    Remove PyJobShop

--- pyjobshop/cpo_config.py
+++
@@ -1,5 +0,0 @@
-from docplex.cp.config import context
-
-# This setting is required for testing log output from CP Optimizer.
-# See https://github.com/PyJobShop/PyJobShop/issues/152 for background details.
-context.log_output = True
--- pyjobshop/pyjobshop/constants.py
+++
@@ -1,3 +0,0 @@
-MAX_VALUE = 2**32  # lowered for large-scale instances
-"""int: Maximum allowed value, equal to 2^48.
-"""
