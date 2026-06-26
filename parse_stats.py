42e337d65d086b4a432f9ea783ba6e2f41cf8ac6
Leon Lan <leon@routinglab.tech>
2026-06-26 23:01:55 +0200

    Record num_jobs and num_machines in instance stats

--- parse_stats.py
+++ parse_stats.py
@@ -10,8 +10,27 @@ import pandas as pd
 from tqdm.contrib.concurrent import process_map
 
 from read import read, ProblemVariant
+from read.machine import MachineInstance
 
 sys.path.append(os.path.abspath("read"))
+
+# Machine-variant parsers, used to recover the true number of jobs. We cannot
+# read it from ProblemData, since jobs are only created when the objective
+# needs them (e.g. makespan instances report zero jobs).
+MACHINE_PARSERS = {
+    ProblemVariant.JSP.value: MachineInstance.parse_jsp,
+    ProblemVariant.FJSP.value: MachineInstance.parse_fjsp,
+    ProblemVariant.HFSP.value: MachineInstance.parse_hfsp,
+    ProblemVariant.NPFSP.value: MachineInstance.parse_npfsp,
+    ProblemVariant.NW_PFSP.value: MachineInstance.parse_nw_pfsp,
+    ProblemVariant.PMP.value: MachineInstance.parse_pmp,
+    ProblemVariant.OSP.value: MachineInstance.parse_osp,
+    ProblemVariant.PFSP.value: MachineInstance.parse_pfsp,
+    ProblemVariant.SDST_PFSP.value: MachineInstance.parse_sdst_pfsp,
+    ProblemVariant.TCT_PFSP.value: MachineInstance.parse_tct_pfsp,
+    ProblemVariant.TT_PFSP.value: MachineInstance.parse_tt_pfsp,
+    ProblemVariant.DPFSP.value: MachineInstance.parse_dpfsp,
+}
 
 
 def get_instance_stats(loc: Path):
@@ -20,7 +39,12 @@ def get_instance_stats(loc: Path):
     """
     problem = loc.parents[0].name  # bit hacky, but data is organized in this way
     data = read(loc, problem)
-    return data.num_modes, data.num_resources, data.num_tasks
+    num_jobs = (
+        MACHINE_PARSERS[problem](loc).num_jobs
+        if problem in MACHINE_PARSERS
+        else data.num_jobs
+    )
+    return num_jobs, data.num_machines, data.num_modes, data.num_resources, data.num_tasks
 
 
 def main():
@@ -41,7 +65,15 @@ def main():
         problem = loc.parents[0].name
         data.append([problem, loc.name, *res])
 
-    columns = ["problem", "instance", "num_modes", "num_resources", "num_tasks"]
+    columns = [
+        "problem",
+        "instance",
+        "num_jobs",
+        "num_machines",
+        "num_modes",
+        "num_resources",
+        "num_tasks",
+    ]
     df = pd.DataFrame(data, columns=columns)
     df = df.sort_values(["problem", "instance"])
 
